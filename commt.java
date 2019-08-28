f7fab6595ee2ddefaa4dea8a6d432f9ed1d94048
small_artisan <1912436696@qq.com>
2019-08-29 00:40:03 +0800

    commuy.java

---
+++ commt.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将打印字符串 Hello World
+     */
+    public static void main(String []args) {
+        System.out.println("Hello World"); // 打印 Hello World
+	System.out.println("Hello World");
+	System.out.println("Hello World1"); // 打印 Hello World
+	System.out.println("Hello World2"); // 打印 Hello World
+    }
+}
